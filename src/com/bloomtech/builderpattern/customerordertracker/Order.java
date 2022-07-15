b7024d73baa175f81de6f384739cfb56495302f1
jcassidy91 <jamie@Lambda-School-Jamies-MacBook-Pro.local>
2022-07-15 14:37:48 -0700

    initial

--- src/com/bloomtech/builderpattern/customerordertracker/Order.java
+++ src/com/bloomtech/builderpattern/customerordertracker/Order.java
@@ -17,68 +17,92 @@ public class Order {
 
     private LocalDate orderplaced; //auto-generated
 
-
-
+    public static OrderBuilder builder() { return new OrderBuilder(); }
 
     //Telescoping constructors...
-    public Order(Item item, String customername, String shippingaddress, String customeremail, String customerphone, String billingaddress, int quantity, String affiliatecode) {
-        this.item = item;
-        this.customername = customername;
-        this.shippingaddress = shippingaddress;
-        this.billingaddress = billingaddress;
+    private Order(OrderBuilder builder) {
+        this.item = builder.item;
+        this.customername = builder.customername;
+        this.shippingaddress = builder.shippingaddress;
+        this.billingaddress = builder.billingaddress;
 
-        this.customeremail = customeremail;
-        this.customerphone = customerphone;
+        this.customeremail = builder.customeremail;
+        this.customerphone = builder.customerphone;
 
-        this.quantity = quantity;
-        this.affiliatecode = affiliatecode;
-        this.orderplaced = LocalDate.now();
+        this.quantity = builder.quantity;
+        this.affiliatecode = builder.affiliatecode;
+        this.orderplaced = builder.orderplaced;
     }
 
-    public Order(Item item, String customername, String shippingaddress, String customeremail, String customerphone, String billingaddress, int quantity) {
-        this.item = item;
-        this.customername = customername;
-        this.shippingaddress = shippingaddress;
-        this.billingaddress = billingaddress;
+    public static class OrderBuilder {
+        private Item item; //required
+        private String customername; //required
+        private String shippingaddress; //required
 
-        this.customeremail = customeremail;
-        this.customerphone = customerphone;
+        private String customeremail;
+        private String customerphone;
+        private String affiliatecode;
 
-        this.quantity = quantity;
-        this.orderplaced = LocalDate.now();
+        private String billingaddress; //defaults to shipping address
+        private int quantity; //defaults to 1
+
+        private LocalDate orderplaced; //auto-generated
+
+        public OrderBuilder() {
+            this.quantity = 1;
+            this.orderplaced = LocalDate.now();
+        }
+
+        public OrderBuilder withItem(Item itemToUse) {
+            this.item = itemToUse;
+            return this;
+        }
+
+        public OrderBuilder withCustomername(String customernameToUse) {
+            this.customername = customernameToUse;
+            return this;
+        }
+
+        public OrderBuilder withShippingAddress(String shippingaddressToUse) {
+            this.shippingaddress = shippingaddressToUse;
+            return this;
+        }
+
+        public OrderBuilder withCustomerEmail(String customeremailToUse) {
+            this.customeremail = customeremailToUse;
+            return this;
+        }
+
+        public OrderBuilder withCustomerPhone(String customerphoneToUse) {
+            this.customerphone = customerphoneToUse;
+            return this;
+        }
+
+        public OrderBuilder withBillingAddress(String billingaddressToUse) {
+            this.billingaddress = billingaddressToUse;
+            return this;
+        }
+
+        public OrderBuilder withQuantity(int quantityToUse) {
+            this.quantity = quantityToUse;
+            return this;
+        }
+
+        public OrderBuilder withAffiliateCode(String affiliateCodeToUse) {
+            this.affiliatecode = affiliateCodeToUse;
+            return this;
+        }
+
+
+        public Order build() {
+            if (item == null) { throw new RuntimeException("Order Item cannot be null!"); }
+            if (customername == null) { throw new RuntimeException("Customer name cannot be null!"); }
+            if (shippingaddress == null) { throw new RuntimeException("Shipping address cannot be null!"); }
+            if (billingaddress == null) { billingaddress = shippingaddress; }
+            return new Order(this);
+        }
     }
-    public Order(Item item, String customername, String shippingaddress, String customeremail, String customerphone, String billingaddress) {
-        this.item = item;
-        this.customername = customername;
-        this.shippingaddress = shippingaddress;
-        this.billingaddress = billingaddress;
 
-        this.customeremail = customeremail;
-        this.customerphone = customerphone;
-
-        this.quantity = 1;
-        this.orderplaced = LocalDate.now();
-    }
-    public Order(Item item, String customername, String shippingaddress, String customeremail) {
-        this.item = item;
-        this.customername = customername;
-        this.shippingaddress = shippingaddress;
-        this.billingaddress = shippingaddress;
-
-        this.customeremail = customeremail;
-
-        this.quantity = 1;
-        this.orderplaced = LocalDate.now();
-    }
-    public Order(Item item, String customername, String shippingaddress) {
-        this.item = item;
-        this.customername = customername;
-        this.shippingaddress = shippingaddress;
-        this.billingaddress = shippingaddress;
-
-        this.quantity = 1;
-        this.orderplaced = LocalDate.now();
-    }
 
 
 
--- tst/com/bloomtech/builderpattern/customerordertracker/OrderBuilderTests.java
+++ tst/com/bloomtech/builderpattern/customerordertracker/OrderBuilderTests.java
@@ -1,87 +1,87 @@
-//package com.bloomtech.builderpattern.customerordertracker;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//import com.bloomtech.builderpattern.customerordertracker.Item;
-//import com.bloomtech.builderpattern.customerordertracker.Order;
-//import org.junit.jupiter.api.Test;
-//
-//import java.lang.reflect.Constructor;
-//import java.time.LocalDate;
-//
-//
-//public class OrderBuilderTests {
-//
-//    @Test
-//    public void instantiateUserWithAllFields_hasValueForEachField()
-//    {
-//        LocalDate currentDate = LocalDate.now();
-//        Order order = Order.builder()
-//                .withItem(Item.GYM_WEAR)
-//                .withShippingAddress("123 Street")
-//                .withBillingAddress("456 Street")
-//                .withCustomerName("test customer")
-//                .withCustomerEmail("tc@email.com")
-//                .withAffiliateCode("1234")
-//                .withQuantity(2)
-//                .withCustomerPhone("123-456-7890")
-//                .build();
-//
-//        //Assert that the order date was placed on the correct day
-//        assertEquals(currentDate.getDayOfYear(), order.getOrderplaced().getDayOfYear());
-//
-//        //Assert that properties are correctly instantiated
-//        assertEquals("test customer", order.getCustomername());
-//
-//        //Assert that the quantity property's default value is being overwritten
-//        assertEquals(2, order.getQuantity());
-//    }
-//
-//    @Test
-//    public void instantiateUserWithMissingOptionalFields_returnsValidUserObject()
-//    {
-//        String shippingAddress = "123 Street";
-//        Order order = Order.builder()
-//                .withItem(Item.GYM_WEAR)
-//                .withShippingAddress(shippingAddress)
-//                .withCustomerName("test customer")
-//                .build();
-//
-//        //Assert that the billing address will default to the shipping address if not provided
-//        assertEquals(shippingAddress, order.getBillingaddress());
-//
-//        //Assert that the com.bloomtech.builderpattern.com.bloomtech.builderpattern.customerordertracker.Order object is valid and contains property data
-//        assertEquals("test customer", order.getCustomername());
-//
-//        //Assert that quantity will default to 1
-//        assertEquals(1, order.getQuantity());
-//    }
-//
-//    @Test
-//    public void instantiateUserWithMissingRequiredFields_throwsRuntimeException()
-//    {
-//        //Assert that a runtime exception occurs if an com.bloomtech.builderpattern.com.bloomtech.builderpattern.customerordertracker.Order is instantiated without required fields
-//        assertThrows(RuntimeException.class, ()->{
-//            Order order = Order.builder()
-//                    .withCustomerName("just a name")
-//                    .build();
-//        });
-//    }
-//
-//    @Test
-//    public void introspectOrderClass_hasNoPublicConstructors()
-//    {
-//        //Assert our class has no public constructors
-//        Class userClass = Order.class;
-//
-//        Constructor[] constructors = userClass.getConstructors();
-//        assertEquals(constructors.length, 0);
-//    }
-//
-//    @Test
-//    public void introspectUserClass_cannotInstantiateWithoutBuilder_throwsInstantiationException() {
-//        assertThrows(InstantiationException.class,()->{
-//            Order.class.newInstance();
-//        });
-//    }
-//}
+package com.bloomtech.builderpattern.customerordertracker;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.bloomtech.builderpattern.customerordertracker.Item;
+import com.bloomtech.builderpattern.customerordertracker.Order;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
+import java.time.LocalDate;
+
+
+public class OrderBuilderTests {
+
+    @Test
+    public void instantiateUserWithAllFields_hasValueForEachField()
+    {
+        LocalDate currentDate = LocalDate.now();
+        Order order = Order.builder()
+                .withItem(Item.GYM_WEAR)
+                .withShippingAddress("123 Street")
+                .withBillingAddress("456 Street")
+                .withCustomername("test customer")
+                .withCustomerEmail("tc@email.com")
+                .withAffiliateCode("1234")
+                .withQuantity(2)
+                .withCustomerPhone("123-456-7890")
+                .build();
+
+        //Assert that the order date was placed on the correct day
+        assertEquals(currentDate.getDayOfYear(), order.getOrderplaced().getDayOfYear());
+
+        //Assert that properties are correctly instantiated
+        assertEquals("test customer", order.getCustomername());
+
+        //Assert that the quantity property's default value is being overwritten
+        assertEquals(2, order.getQuantity());
+    }
+
+    @Test
+    public void instantiateUserWithMissingOptionalFields_returnsValidUserObject()
+    {
+        String shippingAddress = "123 Street";
+        Order order = Order.builder()
+                .withItem(Item.GYM_WEAR)
+                .withShippingAddress(shippingAddress)
+                .withCustomername("test customer")
+                .build();
+
+        //Assert that the billing address will default to the shipping address if not provided
+        assertEquals(shippingAddress, order.getBillingaddress());
+
+        //Assert that the com.bloomtech.builderpattern.com.bloomtech.builderpattern.customerordertracker.Order object is valid and contains property data
+        assertEquals("test customer", order.getCustomername());
+
+        //Assert that quantity will default to 1
+        assertEquals(1, order.getQuantity());
+    }
+
+    @Test
+    public void instantiateUserWithMissingRequiredFields_throwsRuntimeException()
+    {
+        //Assert that a runtime exception occurs if an com.bloomtech.builderpattern.com.bloomtech.builderpattern.customerordertracker.Order is instantiated without required fields
+        assertThrows(RuntimeException.class, ()->{
+            Order order = Order.builder()
+                    .withCustomername("just a name")
+                    .build();
+        });
+    }
+
+    @Test
+    public void introspectOrderClass_hasNoPublicConstructors()
+    {
+        //Assert our class has no public constructors
+        Class userClass = Order.class;
+
+        Constructor[] constructors = userClass.getConstructors();
+        assertEquals(constructors.length, 0);
+    }
+
+    @Test
+    public void introspectUserClass_cannotInstantiateWithoutBuilder_throwsInstantiationException() {
+        assertThrows(InstantiationException.class,()->{
+            Order.class.newInstance();
+        });
+    }
+}
