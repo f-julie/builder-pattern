6bad967f445c3318825d57108f08144903c92c40
Julie Fiess <juliefiess@gmail.com>
2024-12-24 11:09:04 -0600

    Completed

--- src/com/bloomtech/builderpattern/customerordertracker/Order.java
+++ src/com/bloomtech/builderpattern/customerordertracker/Order.java
@@ -17,19 +17,125 @@ public class Order {
 
     private LocalDate orderplaced; //auto-generated
 
-    public Order(Item item, String customername, String shippingaddress, String customeremail, String customerphone, String affiliatecode, String billingaddress, int quantity) {
-        this.item = item;
-        this.customername = customername;
-        this.shippingaddress = shippingaddress;
-        this.billingaddress = billingaddress;
-        
-        this.customeremail = customeremail;
-        this.customerphone = customerphone;
-        
-        this.quantity = quantity;
-        this.affiliatecode = affiliatecode;
-        this.orderPlaced = LocalDate.now()
+    public static OrderBuilder builder() { return new OrderBuilder(); }
+
+    private Order(OrderBuilder builder) {
+        this.item = builder.item;
+        this.customername = builder.customername;
+        this.shippingaddress = builder.shippingaddress;
+        this.billingaddress = builder.billingaddress;
+
+        this.customeremail = builder.customeremail;
+        this.customerphone = builder.customerphone;
+
+        this.quantity = builder.quantity;
+        this.affiliatecode = builder.affiliatecode;
+        this.orderplaced = builder.orderplaced;
+    }
+
+    public static class OrderBuilder {
+        private Item item; //required
+        private String customername; //required
+        private String shippingaddress; //required
+
+        private String customeremail;
+        private String customerphone;
+        private String affiliatecode;
+
+        private String billingaddress; //defaults to shipping address
+        private int quantity; //defaults to 1
+
+        private LocalDate orderplaced; //auto-generated
+
+        public OrderBuilder() {
+            this.quantity = 1;
+            this.orderplaced = LocalDate.now();
+        }
+
+        public OrderBuilder withItem(Item itemToUse) {
+            this.item = itemToUse;
+            return this;
+        }
+
+        public OrderBuilder withCustomername(String customernameToUse) {
+            this.customername = customernameToUse;
+            return this;
+        }
+
+        public OrderBuilder withShippingAddress(String shippingaddressToUse) {
+            this.shippingaddress = shippingaddressToUse;
+            return this;
+        }
+
+        public OrderBuilder withCustomerEmail(String customeremailToUse) {
+            this.customeremail = customeremailToUse;
+            return this;
+        }
+
+        public OrderBuilder withCustomerPhone(String customerphoneToUse) {
+            this.customerphone = customerphoneToUse;
+            return this;
+        }
+
+        public OrderBuilder withAffiliateCode(String affiliatecodeToUse) {
+            this.affiliatecode = affiliatecodeToUse;
+            return this;
+        }
+
+        public OrderBuilder withBillingAddress(String billingaddressToUse) {
+            this.billingaddress = billingaddressToUse;
+            return this;
+        }
+
+        public OrderBuilder withQuantity(int quantityToUse) { // Can override default of 1
+            this.quantity = quantityToUse;
+            return this;
+        }
+
+        public Order build() {
+            if (item == null) { throw new RuntimeException("Order item cannot be null."); }
+            if (customername == null) { throw new RuntimeException("Customer name cannot be null."); }
+            if (shippingaddress == null) { throw new RuntimeException("Shipping address cannot be null."); }
+            if (billingaddress == null) { billingaddress = shippingaddress; }
+            return new Order(this);
+        }
+    }
 
 
+
+    public Item getItem() {
+        return item;
+    }
+
+    public String getCustomername() {
+        return customername;
+    }
+
+    public String getShippingaddress() {
+        return shippingaddress;
+    }
+
+    public String getCustomeremail() {
+        return customeremail;
+    }
+
+    public String getCustomerphone() {
+        return customerphone;
+    }
+
+    public String getAffiliatecode() {
+        return affiliatecode;
+    }
+
+    public String getBillingaddress() {
+        return billingaddress;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public LocalDate getOrderplaced() {
+        return orderplaced;
     }
 }
